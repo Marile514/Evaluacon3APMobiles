962e3252fd59fd70e05d121670b6b42ef7c24b3c
Marile514 <73203639+Marile514@users.noreply.github.com>
2021-07-21 17:29:03 -0400

    adminSQL modificado

--- app/src/main/java/com/example/evaluacon3apmobiles/AdminSQLiteUsers.java
+++ app/src/main/java/com/example/evaluacon3apmobiles/AdminSQLiteUsers.java
@@ -8,7 +8,12 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 
 public class AdminSQLiteUsers extends SQLiteOpenHelper {
+    final String users_table = "CREATE TABLE usuario (id_Usuario INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "nombre_apellido TEXT, username TEXT, correo TEXT, password TEXT, pregunta TEXT, respuesta TEXT)";
 
+    final String events_table = "CREATE TABLE evento (id_evento INT PRIMARY KEY AUTOINCREMENT, titulo TEXT, " +
+            "importancia TEXT, observacion TEXT, lugar TEXT, fecha_evento TEXT, tiempo_aviso TEXT, id_Usuario INTEGER NOT NULL," +
+            "CONSTRAINT FK_USUARIO FOREIGN KEY (id_usuario) REFERENCES usuario (id_Usuario))";
 
     public AdminSQLiteUsers(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -16,8 +21,8 @@ public class AdminSQLiteUsers extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(Utilidades.users_table);
-        db.execSQL(Utilidades.events_table);
+        db.execSQL(users_table);
+        db.execSQL(events_table);
         Log.d("TAG_","Base de datos creado");
     }
 
